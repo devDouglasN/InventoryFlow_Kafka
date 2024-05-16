a2ecdf0fdc91341bc648865cdae6ccf5cea2e660
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-16 14:58:02 -0300

    Kafka Producer Configuration

---
+++ producer/src/main/java/com/douglas/producer/config/KafkaProducerConfig.java
@@ -0,0 +1,41 @@
+package com.douglas.producer.config;
+
+import com.douglas.producer.dto.StockDTO;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KafkaProducerConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Value(value = "${topic.name}")
+    private String topic;
+
+    @Bean
+    public NewTopic createTopic() {
+        return new NewTopic(topic, 3, (short) 1);
+    }
+
+    private ProducerFactory<String, StockDTO> producer() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, StockDTO> kafkaTemplate() {
+        return new KafkaTemplate<>(producer());
+    }
+}
