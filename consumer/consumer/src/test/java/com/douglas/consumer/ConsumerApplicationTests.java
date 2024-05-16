c7b1650d47e856d85e7c3b4bc7dc37ef6b1c79df
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-16 15:36:23 -0300

    Consumer created

---
+++ consumer/consumer/src/test/java/com/douglas/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
