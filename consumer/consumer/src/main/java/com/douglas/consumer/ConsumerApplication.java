5a7e508209ae7b4df4cf5d80d1129db0dcd220b0
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-16 15:46:22 -0300

    Connecting to Kafka for Consumer

--- consumer/consumer/src/main/java/com/douglas/consumer/ConsumerApplication.java
+++ consumer/consumer/src/main/java/com/douglas/consumer/ConsumerApplication.java
@@ -2,8 +2,10 @@ package com.douglas.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
