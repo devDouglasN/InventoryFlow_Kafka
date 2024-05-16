3609e3e54d230e7d85026a01e44578944705989e
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-16 14:27:16 -0300

    Local connection with Kafka

--- producer/src/main/java/com/douglas/producer/ProducerApplication.java
+++ producer/src/main/java/com/douglas/producer/ProducerApplication.java
@@ -2,8 +2,10 @@ package com.douglas.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class ProducerApplication {
 
 	public static void main(String[] args) {
