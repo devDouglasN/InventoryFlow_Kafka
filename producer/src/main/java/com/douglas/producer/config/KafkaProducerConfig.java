1f3eb8a6a9f9f722001191ed6c027d03ddf6a825
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-17 12:15:38 -0300

    Spring configure annotation

--- producer/src/main/java/com/douglas/producer/config/KafkaProducerConfig.java
+++ producer/src/main/java/com/douglas/producer/config/KafkaProducerConfig.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -13,6 +14,7 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+@Configuration
 public class KafkaProducerConfig {
 
     @Value(value = "${spring.kafka.bootstrap-servers}")
