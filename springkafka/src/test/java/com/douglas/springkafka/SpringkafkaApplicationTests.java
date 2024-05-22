90563b20a927784ece6bdcd94faa34fe3a4ec3c4
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-22 15:02:18 -0300

    SpringKafka project created

---
+++ springkafka/src/test/java/com/douglas/springkafka/SpringkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.springkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
