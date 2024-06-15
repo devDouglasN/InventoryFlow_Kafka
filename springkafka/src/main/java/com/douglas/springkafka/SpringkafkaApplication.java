3f50d5d8d7638e1ec989f7e96c686d062df70f4b
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-06-14 23:25:33 -0300

    Log for verification with bank

--- springkafka/src/main/java/com/douglas/springkafka/SpringkafkaApplication.java
+++ springkafka/src/main/java/com/douglas/springkafka/SpringkafkaApplication.java
@@ -1,13 +1,24 @@
 package com.douglas.springkafka;
 
+import jakarta.persistence.EntityManager;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringkafkaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringkafkaApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(EntityManager entityManager) {
+		return args -> {
+			entityManager.createQuery("SELECT 1").getSingleResult();
+			System.out.println("Conexão com o banco de dados H2 foi bem-sucedida!");
+		};
 	}
 
 }
