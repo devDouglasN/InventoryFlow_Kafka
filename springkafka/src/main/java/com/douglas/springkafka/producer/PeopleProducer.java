3c934f4b9651fec6461c7cb2242c1a6ffac8dd39
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-06-15 15:30:22 -0300

    Correction in the message sending method

--- springkafka/src/main/java/com/douglas/springkafka/producer/PeopleProducer.java
+++ springkafka/src/main/java/com/douglas/springkafka/producer/PeopleProducer.java
@@ -1,8 +1,12 @@
 package com.douglas.springkafka.producer;
 
+import br.com.springkafka.People;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Slf4j
 @Component
@@ -11,16 +15,19 @@ public class PeopleProducer {
     private final String topicName;
     private final KafkaTemplate<String, People> kafkaTemplate;
 
-
     public PeopleProducer(@Value("${topic.name}") String topicName, KafkaTemplate<String, People> kafkaTemplate) {
         this.topicName = topicName;
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendMessage (People people) {
-        kafkaTemplate.send(topicName.people).addCallBack(
-                sucess -> log.info("Mensagem enviada com sucesso!"),
-                sucess -> log.info("Falha ao enviar mensagem!")
-        );
+    public void sendMessage(People people) {
+        kafkaTemplate.send(topicName, people).whenComplete((result, ex) -> {
+            if (ex == null) {
+                log.info("Mensagem enviada com sucesso para o offset {}", result.getRecordMetadata().offset());
+            } else {
+                log.error("Falha ao enviar mensagem", ex);
+            }
+        });
     }
+
 }
