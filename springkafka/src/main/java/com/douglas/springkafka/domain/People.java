c0dacbcc0fa5919e3a4323a51774e11d5a05154f
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-06-15 15:26:51 -0300

    People entity created

---
+++ springkafka/src/main/java/com/douglas/springkafka/domain/People.java
@@ -0,0 +1,23 @@
+package com.douglas.springkafka.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.List;
+
+@Entity
+public class People {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    private String name;
+    private String cpf;
+
+    @OneToMany(mappedBy = "person")
+    private List<Stock> stocks;
+}
